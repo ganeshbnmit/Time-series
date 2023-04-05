e65e68889b1e5794c3784aadf0ab6fad3bedd748
ganeshbnmit <103561028+ganeshbnmit@users.noreply.github.com>
2023-04-05 18:23:36 +0530

    Add files via upload

---
+++ Radom_forest.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import pandas as pd
+from pmdarima.arima import auto_arima
+from sktime.performance_metrics.forecasting import mean_absolute_percentage_error as mean_absolute_error
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+from sktime.forecasting.model_selection import temporal_train_test_split
+import seaborn as sns 
+import matplotlib.pyplot as plt 
+import statsmodels.api as sm
+from statsmodels.tsa.stattools import adfuller, kpss
+from prophet import Prophet
+from sktime.forecasting.base import ForecastingHorizon
+from sktime.forecasting.compose import make_reduction
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
+from sktime.forecasting.naive import NaiveForecaster
+from sktime.performance_metrics.forecasting import mean_absolute_percentage_error as smape_loss
+from sktime.forecasting.arima import AutoARIMA
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.preprocessing import scale
+from sklearn.ensemble import RandomForestRegressor
+
+
+# In[2]:
+
+
+class TimeSeriesAnalysis:
+    def Randomforest(self):
+        data = pd.read_csv(r"C:\Users\admin\Downloads\Electric_Production.csv")
+        productions = data['IPG2211A2N'].values
+        productions = scale(productions)
+
+        last_days = []
+        today = []
+        step = 1
+        max_len = 5
+        for idx in range(0, len(productions) - max_len, step):
+            last_days.append(productions[idx: idx + max_len])
+            today.append(productions[idx + max_len])
+
+        x = np.array(last_days)
+        y = np.array(today)
+
+        split = x.shape[0] // 5
+        X_train = x[:-split]
+        X_test = x[-split:]
+        y_train = y[:-split]
+        y_test = y[-split:]
+
+
+        regr = RandomForestRegressor(random_state=42, n_estimators=50)
+
+        regr.fit(X_train, y_train)
+        y_pred = regr.predict(X_test)
+        
+        ant = X_test.shape[0] + 300
+        x_axis = np.arange(ant) + len(productions) - ant
+        x_axis_nxt = np.arange(len(y_pred)) + y_train.shape[0]
+        plt.plot(x_axis, productions[-ant:], linewidth=2)
+        plt.plot(x_axis_nxt, y_pred, linewidth=2)
+        plt.show()
+        
+    
+   
+   
+  
+
+    
+             
+
+
+# In[3]:
+
+
+production = TimeSeriesAnalysis()
+#production.get_time_series('IPG2211A2N')
+production.Randomforest()
+
+
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ s_k_time.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[94]:
+
+
+from sktime.datasets import load_airline
+from sktime.forecasting.base import ForecastingHorizon
+from sktime.forecasting.naive import NaiveForecaster
+from sktime.utils.plotting import plot_series
+from sktime.forecasting.model_selection import temporal_train_test_split
+from sktime.performance_metrics.forecasting import mean_absolute_percentage_error
+from sktime.forecasting.arima import ARIMA
+from sktime.forecasting.compose import TransformedTargetForecaster
+from sktime.transformations.series.detrend import Deseasonalizer
+from sklearn.neighbors import KNeighborsRegressor
+from sktime.forecasting.compose import make_reduction
+from sktime.forecasting.trend import PolynomialTrendForecaster
+from sktime.transformations.series.detrend import Detrender
+from sktime.forecasting.compose import make_reduction
+from xgboost import XGBRegressor
+import datetime as dt
+import json
+import numpy as np
+import pandas as pd
+from datetime import datetime
+y = load_airline()
+#y = pd.read_csv(r"C:\Users\admin\Downloads\Electric_Production.csv")
+#y['DATE']=pd.to_datetime(y['DATE'],infer_datetime_format=True)
+#indexedy = y.set_index(['DATE'])
+
+
+# In[ ]:
+
+
+
+
+
+# In[97]:
+
+
+class TimeSeriesAnalysis:
+    def forecast_naive(self,timeseries_data, horizon, seasonality_period):
+        y = timeseries_data
+        fh = np.arange(1, horizon+1)
+        forecaster = NaiveForecaster(strategy="last", sp=seasonality_period)
+        forecaster.fit(y, fh=fh)
+        y_pred = forecaster.predict()
+        plot_series(y, y_pred, labels=["y", "y_pred"])
+    
+    def forecast_kneighbors_test(self,timeseries_data, test_size):
+        y = timeseries_data
+        y_train, y_test = temporal_train_test_split(y, test_size=test_size)
+        fh = ForecastingHorizon(y_test.index, is_relative=False)
+
+        regressor = KNeighborsRegressor(n_neighbors=1)
+        forecaster = make_reduction(regressor, window_length=15, strategy="recursive")
+        forecaster.fit(y_train)
+        y_pred = forecaster.predict(fh)
+        plot_series(y_train, y_test, y_pred, labels=["y_train", "y_test", "y_pred"])
+        print("Mean Absolute Percentage Error (MAPE):", mean_absolute_percentage_error(y_test, y_pred, symmetric=False))
+        
+    def forecast_arima_test(self,timeseries_data, test_size):
+        y = timeseries_data
+        y_train, y_test = temporal_train_test_split(y, test_size=test_size)
+        fh = ForecastingHorizon(y_test.index, is_relative=False)
+
+        forecaster = ARIMA(
+            order=(1, 1, 0), seasonal_order=(0, 1, 0, 12), suppress_warnings=True
+        )
+
+        forecaster.fit(y_train)
+        y_pred = forecaster.predict(fh)
+        plot_series(y_train, y_test, y_pred, labels=["y_train", "y_test", "y_pred"])
+        print("Mean Absolute Percentage Error (MAPE):", mean_absolute_percentage_error(y_test, y_pred, symmetric=False))
+        
+    def xgboost(self,timeseries_data, test_size):
+        y = timeseries_data
+        y_train, y_test = temporal_train_test_split(y, test_size=test_size)
+        fh = ForecastingHorizon(y_test.index, is_relative=False)
+        
+        regressor = XGBRegressor(objective='reg:squarederror', random_state=42)
+
+        forecaster = make_reduction(regressor, strategy="recursive", window_length=52)
+        forecaster.fit(y_train)
+        y_pred = forecaster.predict(fh)
+        plot_series(y_train, y_test, y_pred, labels=["y_train", "y_test", "y_pred"])
+        print("Mean Absolute Percentage Error (MAPE):", mean_absolute_percentage_error(y_test, y_pred, symmetric=False))
+
+
+# In[98]:
+
+
+passengers=TimeSeriesAnalysis()
+passengers.forecast_arima_test(timeseries_data=y, test_size=36)
+
+
+# In[99]:
+
+
+passengers=TimeSeriesAnalysis()
+passengers.forecast_naive(timeseries_data=y, horizon=36, seasonality_period=12)
+
+
+# In[100]:
+
+
+passengers=TimeSeriesAnalysis()
+passengers.forecast_kneighbors_test(timeseries_data=y, test_size=36)
+
+
+# In[93]:
+
+
+passengers=TimeSeriesAnalysis()
+passengers.xgboost(timeseries_data=y, test_size=36)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
